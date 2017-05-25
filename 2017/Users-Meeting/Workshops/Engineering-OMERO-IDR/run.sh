708287b7985a3c1dd3cf780ebcaab99b2b183987
Simon Li <spli@dundee.ac.uk>
2017-05-25 17:40:14 +0100

    More updates

--- 2017/Users-Meeting/Workshops/Engineering-OMERO-IDR/run.sh
+++ 2017/Users-Meeting/Workshops/Engineering-OMERO-IDR/run.sh
@@ -1,6 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 # Run the node server on http://localhost:8000
 # Hit <s> to bring up the presenter view
-npm install
+[ "$(ls -A node_modules)" ] || npm install
 npm start
-
